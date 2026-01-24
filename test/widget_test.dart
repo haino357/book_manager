176748fc2721d6fcbb5f98e9dc801deff8344bb7
ShojiTakayuki <tosho427.sysdiv@gmail.com>
2026-01-24 20:34:53 +0900

    Riverpodパッケージのバージョン統一とテスト修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,16 @@
 import 'package:book_manager/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
